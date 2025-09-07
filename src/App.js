ac5fe049987483c067f534360b2655d5d93ca726
mat9919 <matter20th@gmail.com>
2025-09-06 23:17:29 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmployeeTimecardApp from './components/EmployeeTimecardApp';
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeTimecardApp />
+    </div>
+  );
+}
+
+export default App;
