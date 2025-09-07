634257f1ac29dc9a89ffd5c14f2898afad7f9e2a
mat9919 <matter20th@gmail.com>
2025-09-07 00:44:01 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee Time Card App</h1>
+    </div>
+  );
+}
+
+export default App; // 👈 ต้องมีบรรทัดนี้!
