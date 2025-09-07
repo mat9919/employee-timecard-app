32fd3820a56aad94521662d72189e0a0764b04b7
mat9919 <matter20th@gmail.com>
2025-09-07 00:26:21 -0700

    Create EmployeeTimeCard.jsx

---
+++ src/components/EmployeeTimeCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function EmployeeTimeCard() {
+  return (
+    <div className="timecard">
+      <h2>Employee Time Card</h2>
+      <p>Check-in: 08:00 AM</p>
+      <p>Check-out: 05:00 PM</p>
+    </div>
+  );
+}
+
+export default EmployeeTimeCard;
